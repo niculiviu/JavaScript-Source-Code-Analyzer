f74880f1b429a07ab8d95e6e765c9efde38d3fe5
liviunicu <liviu-alexandru.nicu@my.fmi.unibuc.ro>
2015-03-17 23:13:56 +0200

    learning...

--- js/angular/controllers/indexController.js
+++ js/angular/controllers/indexController.js
@@ -2,5 +2,11 @@
 
 app.controller('indexCtrl', ['$scope', '$rootScope','indexService',
     function ($scope, $rootScope,indexService) {
+        
+        $scope.analyze=function(code){
+            console.log(code.split(" "));
+            console.log(code.split(""));
+            console.log(code.replace(/\s/g, ""));
+        }
        
     }]);
