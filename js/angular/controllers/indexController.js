63a7b3d0744a682382382057232ae063aafaa406
liviunicu <liviu-alexandru.nicu@my.fmi.unibuc.ro>
2015-03-21 19:52:52 +0200

    cateva corectari

--- js/angular/controllers/indexController.js
+++ js/angular/controllers/indexController.js
@@ -5,18 +5,52 @@ app.controller('indexCtrl', ['$scope', '$rootScope','indexService',
         $scope.semne=["==", "=", "+", "-", "*", "/", "%", "++", "--", "!=", "not_eq", "<", "<=", ">", ">=", "!", "not", "&&", "and", "||", "or", "~", "&", "|", "^", "xor", "<<", ">>", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "<<=", ">>=", "->", "}", "{", ")", "(", "[", "]"];
         $scope.cuvinte_cheie=["abstract", "array", "auto", "bool", "break", "case", "catch", "char", "class", "const", "const_cast", "continue", "decltype", "default", "delegate", "delete", "deprecated 1", "dllexport", "do", "double", "dynamic_cast", "else", "enum", "enum class", "enum struct", "event", "explicit", "extern", "false", "finally", "float", "for", "for each, in", "friend", "friend_as", "gcnew", "generic", "goto", "if", "initonly", "inline", "int", "interface class", "interface struct", "interior_ptr", "literal", "long", "mutable", "naked 1", "namespace", "new", "new", "noinline 1", "noreturn 1", "nothrow 1", "novtable 1", "nullptr", "operator", "private", "property", "property 1", "protected", "public", "ref class", "ref struct", "register", "reinterpret_cast", "return", "safecast", "sealed", "selectany 1", "short", "signed", "sizeof", "static", "static_assert", "static_cast", "struct", "switch", "template", "this", "thread 1", "throw", "true", "try", "typedef", "typeid", "typeid", "typename", "union", "unsigned", "using declaration, using directive", "uuid 1", "value class", "value struct", "virtual", "void", "volatile", "while"];
         $scope.variabile=[];
+        
         $scope.analyze=function(code){
-            var cuvinte_cheie=["","",""];
-            var semne_de_punctuatie=["","",""];
-            var array = code.split("");
-            console.log(array);
+            var semne=["==", "=", "+", "-", "*", "/", "%", "++", "--", "!=", "not_eq", "<", "<=", ">", ">=", "!", "not", "&&", "and", "||", "or", "~", "&", "|", "^", "xor", "<<", ">>", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "<<=", ">>=", "->", "}", "{", ")", "(", "[", "]"," ",",",";",""];
+        var cuvinte_cheie=["abstract", "array", "auto", "bool", "break", "case", "catch", "char", "class", "const", "const_cast", "continue", "decltype", "default", "delegate", "delete", "deprecated 1", "dllexport", "do", "double", "dynamic_cast", "else", "enum", "enum class", "enum struct", "event", "explicit", "extern", "false", "finally", "float", "for", "for each, in", "friend", "friend_as", "gcnew", "generic", "goto", "if", "initonly", "inline", "int", "interface class", "interface struct", "interior_ptr", "literal", "long", "mutable", "naked 1", "namespace", "new", "new", "noinline 1", "noreturn 1", "nothrow 1", "novtable 1", "nullptr", "operator", "private", "property", "property 1", "protected", "public", "ref class", "ref struct", "register", "reinterpret_cast", "return", "safecast", "sealed", "selectany 1", "short", "signed", "sizeof", "static", "static_assert", "static_cast", "struct", "switch", "template", "this", "thread 1", "throw", "true", "try", "typedef", "typeid", "typeid", "typename", "union", "unsigned", "using declaration, using directive", "uuid 1", "value class", "value struct", "virtual", "void", "volatile", "while","cin","cout"];
+            
+            var vector_ajutator=[];
+            console.log(code.replace(/\r\n|\n/g, ''));
+            var array = code.replace(/\r\n|\n/g, '').split("");
+            var aux='';
             
             for(var i=0;i<array.length;i++){
-                if(array[i] in semne_de_punctuatie){
-                        $scope.
+                
+                if(semne.indexOf(array[i]) > -1){
+                    if(array[i]!=" "){
+                            $scope.semne.push({val:array[i]});
+                        }
+                        vector_ajutator.push(aux);
+                        aux='';
+                }else{
+                    aux=aux.concat(array[i]);
                 }
             }
-
+            console.log(vector_ajutator);
+            for(var i=0;i<vector_ajutator.length;i++){
+                var vizitat=0;
+                for(var j=0;j<cuvinte_cheie.length;j++){
+                    if(vector_ajutator[i]==cuvinte_cheie[j]){
+                    $scope.cuvinte_cheie.push({val:vector_ajutator[i]});
+                    }
+                    else
+                    {
+                        if(vizitat==0){
+                            if(parseInt(vector_ajutator[i])){
+                                if(vector_ajutator[i]!='\t')
+                                    $scope.variabile.push({val:vector_ajutator[i],tip:'number'});
+                            }else{
+                                if(vector_ajutator[i]!='\t')
+                                    $scope.variabile.push({val:vector_ajutator[i],tip:'string'});
+                            }
+                        }
+                        vizitat=1;
+                    }
+                }
+               
+            }
+        console.log($scope.variabile);
         }
        
     }]);
