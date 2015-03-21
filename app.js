cc5ff2477f98110c6c4242621ed9bae93f568843
liviunicu <liviu-alexandru.nicu@my.fmi.unibuc.ro>
2015-03-21 20:25:32 +0200

    display unique

--- app.js
+++ app.js
@@ -10,3 +10,20 @@ app.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider.when('/', { templateUrl: 'template/main.html', controller: 'indexCtrl' });
     $routeProvider.otherwise({ redirectTo: '/404' });
 }]);
+
+app.filter('unique', function() {
+   return function(collection, keyname) {
+      var output = [], 
+          keys = [];
+
+      angular.forEach(collection, function(item) {
+          var key = item[keyname];
+          if(keys.indexOf(key) === -1) {
+              keys.push(key);
+              output.push(item);
+          }
+      });
+
+      return output;
+   };
+});
