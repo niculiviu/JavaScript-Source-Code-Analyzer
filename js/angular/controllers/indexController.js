fc590ed6333fb8be4bc8c300acf09cd4ab48bbaf
liviunicu <liviu-alexandru.nicu@my.fmi.unibuc.ro>
2015-03-21 22:59:54 +0200

    operatori impartiti pe categorii

--- js/angular/controllers/indexController.js
+++ js/angular/controllers/indexController.js
@@ -26,9 +26,46 @@ app.controller('indexCtrl', ['$scope', '$rootScope','indexService',
             var vector_ajutator=[];
             var only_math=code.replace(/([a-zA-Z ]|[0-9])|\s/g, " ").split(" ").clean('')
             for(var i=0;i<only_math.length;i++){
-                only_math[i]=only_math[i].replace(';','');
+                //only_math[i]=only_math[i].replace(';','');
                 if(semne.indexOf(only_math[i]) > -1){
-                    $scope.semne.push({val:only_math[i]});
+                    if(
+                        only_math[i]==semne[1]||
+                        only_math[i]==semne[2]||
+                        only_math[i]==semne[3]||
+                        only_math[i]==semne[4]||
+                        only_math[i]==semne[5]||
+                        only_math[i]==semne[6]||
+                        only_math[i]==semne[7]||
+                        only_math[i]==semne[8]){
+                        $scope.semne.push({val:only_math[i],tip:'Operator matematic'});
+                    }if(
+                         only_math[i]==semne[9]||
+                         only_math[i]==semne[10]||
+                         only_math[i]==semne[11]||
+                         only_math[i]==semne[12]||
+                         only_math[i]==semne[13]||
+                         only_math[i]==semne[14]||
+                         only_math[i]==semne[15]||
+                         only_math[i]==semne[16]||
+                         only_math[i]==semne[17]||
+                         only_math[i]==semne[18]||
+                         only_math[i]==semne[19]||
+                         only_math[i]==semne[20]||
+                         only_math[i]==semne[21]||
+                         only_math[i]==semne[22]||
+                         only_math[i]==semne[23]||
+                         only_math[i]==semne[24]||
+                         only_math[i]==semne[25]||
+                         only_math[i]==semne[26]||
+                         only_math[i]==semne[27]||
+                         only_math[i]==semne[28]||
+                         only_math[i]==semne[29]){
+                            $scope.semne.push({val:only_math[i],tip:'Operator logic'});
+                    }
+                    else{
+                        $scope.semne.push({val:only_math[i],tip:'Separator'});
+                    }
+                    
                 }
             }
             $scope.semne.clean('');
@@ -39,9 +76,6 @@ app.controller('indexCtrl', ['$scope', '$rootScope','indexService',
             for(var i=0;i<array.length;i++){
                 
                 if(semne.indexOf(array[i]) > -1){
-                    if(array[i]!=" "){
-                            //$scope.semne.push({val:array[i]});
-                        }
                         vector_ajutator.push(aux);
                         aux='';
                 }else{
